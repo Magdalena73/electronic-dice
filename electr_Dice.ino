009f844ff45a8e1e94d7d0c8096c36f23a85a4b5
Magdalena73 <magdalena73@gmx.de>
2020-05-19 18:42:35 +0200

    Add files via upload

--- electr_Dice.ino
+++ electr_Dice.ino
@@ -1,24 +1,26 @@
 /*MStronik♥Arduino♥Electronic dice
  * https://mstronik.blog/2018/10/19/electronic-dice/
- * Simulierung von einme elektronisch Würfel
- * mit Rendom-Funktion
+ * Simulierung von einem elektronische Würfel
+ * mit Random-Funktion
  */
 int pushButton = 2;
-int pin1_4 = 13;
-int pin2_5 = 11;
-int pin3_6 = 10;
-int pin7 = 12;
+int LED_1_4 = 13;
+int LED_2_5 = 11;
+int LED_3_6 = 10;
+int LED_7 = 12;
 int val_Dice = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pin1_4, OUTPUT);
-  pinMode(pin2_5, OUTPUT);
-  pinMode(pin3_6, OUTPUT);
-  pinMode(pin7, OUTPUT);
+  pinMode(LED_1_4, OUTPUT);
+  pinMode(LED_2_5, OUTPUT);
+  pinMode(LED_3_6, OUTPUT);
+  pinMode(LED_7, OUTPUT);
   pinMode(pushButton, INPUT);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 }
+
+//**** MAIN ***************************
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -27,7 +29,55 @@ void loop() {
  {
   rolling_dice();
   val_Dice = random (1,7);
-  switch (val_Dice){
+  show_number(val_Dice);
+  }
+}
+
+//******************* FUNCTIONS ***********************************************  
+void show_one(){
+    digitalWrite(LED_7, HIGH);
+    digitalWrite(LED_1_4, LOW);
+    digitalWrite(LED_2_5, LOW);
+    digitalWrite(LED_3_6, LOW);
+    }
+    
+void show_two(){
+    digitalWrite(LED_1_4, HIGH);
+    digitalWrite(LED_7, LOW);
+    digitalWrite(LED_2_5, LOW);
+    digitalWrite(LED_3_6, LOW);
+    }
+    
+void show_three(){
+    digitalWrite(LED_1_4, HIGH);
+    digitalWrite(LED_7, HIGH);
+    digitalWrite(LED_2_5, LOW);
+    digitalWrite(LED_3_6, LOW);
+    }
+    
+void show_four(){
+    digitalWrite(LED_1_4, HIGH);
+    digitalWrite(LED_3_6, HIGH);
+    digitalWrite(LED_7, LOW);
+    digitalWrite(LED_2_5, LOW);
+    }
+  
+void show_five(){
+    digitalWrite(LED_1_4, HIGH);
+    digitalWrite(LED_7, HIGH);
+    digitalWrite(LED_3_6, HIGH);
+    digitalWrite(LED_2_5, LOW);
+    } 
+void show_six(){
+    digitalWrite(LED_1_4, HIGH);
+    digitalWrite(LED_2_5, HIGH);
+    digitalWrite(LED_3_6, HIGH);
+    digitalWrite(LED_7, LOW);
+   }
+
+//******** show number function ************************
+void show_number(int num){
+     switch (num){
     case 1:
       show_one();
       break;
@@ -45,88 +95,23 @@ void loop() {
       break;
     case 6:
       show_six();
-      break;  
+      break;
+    default:
+      break;    
    }
-  }
 }
-//******************* FUNCTIONS ***********************************************  
-void show_one(){
-    digitalWrite(pin7, HIGH);
-    digitalWrite(pin1_4, LOW);
-    digitalWrite(pin2_5, LOW);
-    digitalWrite(pin3_6, LOW);
-    }
-    
-void show_two(){
-    digitalWrite(pin1_4, HIGH);
-    digitalWrite(pin7, LOW);
-    digitalWrite(pin2_5, LOW);
-    digitalWrite(pin3_6, LOW);
-    }
-    
-void show_three(){
-    digitalWrite(pin1_4, HIGH);
-    digitalWrite(pin7, HIGH);
-    digitalWrite(pin2_5, LOW);
-    digitalWrite(pin3_6, LOW);
-    }
-    
-void show_four(){
-    digitalWrite(pin1_4, HIGH);
-    digitalWrite(pin3_6, HIGH);
-    digitalWrite(pin7, LOW);
-    digitalWrite(pin2_5, LOW);
-    }
-  
-void show_five(){
-    digitalWrite(pin1_4, HIGH);
-    digitalWrite(pin7, HIGH);
-    digitalWrite(pin3_6, HIGH);
-    digitalWrite(pin2_5, LOW);
-    } 
-void show_six(){
-    digitalWrite(pin3_6, HIGH);
-    digitalWrite(pin7, LOW);
-    digitalWrite(pin2_5, HIGH);
-    digitalWrite(pin1_4, HIGH);
-   }
-//********** roling dice function ************************ 
+//********** rolling dice function ************************ 
  
  void rolling_dice()
- {
-  digitalWrite(pin1_4, HIGH);
-  digitalWrite(pin2_5, LOW);
-  digitalWrite(pin3_6, LOW);
-  digitalWrite(pin7, LOW);
-  delay(100);
-  digitalWrite(pin1_4, LOW);
-  digitalWrite(pin2_5, HIGH);
-  digitalWrite(pin3_6, LOW);
-  digitalWrite(pin7, LOW);
-  delay(100);
-  digitalWrite(pin1_4, LOW);
-  digitalWrite(pin2_5, LOW);
-  digitalWrite(pin3_6, HIGH);
-  digitalWrite(pin7, LOW);
-  delay(100);
-  digitalWrite(pin1_4, LOW);
-  digitalWrite(pin2_5, LOW);
-  digitalWrite(pin3_6, LOW);
-  digitalWrite(pin7, HIGH);
-  delay(100);
-  digitalWrite(pin1_4, LOW);
-  digitalWrite(pin2_5, LOW);
-  digitalWrite(pin3_6, HIGH);
-  digitalWrite(pin7, LOW);
-  delay(100);
-  digitalWrite(pin1_4, LOW);
-  digitalWrite(pin2_5, HIGH);
-  digitalWrite(pin3_6, LOW);
-  digitalWrite(pin7, LOW);
-  delay(100);
-  digitalWrite(pin1_4, HIGH);
-  digitalWrite(pin2_5, LOW);
-  digitalWrite(pin3_6, LOW);
-  digitalWrite(pin7, LOW);
-  delay(100);
+ {  int i;
+    for (i=1; i>7; i++){
+      show_number(i);
+      delay(100);
+    }
+    for (i=6; i>0; i--){
+      show_number(i);
+      delay(100);
+    }
+  
   }
+  
